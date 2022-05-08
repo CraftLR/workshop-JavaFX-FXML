3cd47ad4cb55b9b4f0851de60a4de32730cb025b
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2022-05-08 11:55:10 +0200

    Mise à jour de mockito

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 open module tp.fxml {
-    requires javafx.base;
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires javafx.fxml;
+    requires transitive javafx.base;
+    requires transitive javafx.controls;
+    requires transitive javafx.graphics;
+    requires transitive javafx.fxml;
 
     exports fr.univ_amu.iut;
     exports fr.univ_amu.iut.exercice1;
