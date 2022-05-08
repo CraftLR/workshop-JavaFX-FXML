7e95959aea0660851814d076bcdf48e8cc68c831
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2022-05-08 11:51:15 +0200

    Ajout des classes de test

---
+++ src/test/java/fr/univ_amu/iut/exercice1/CounterMainTest.java
@@ -0,0 +1,7 @@
+package fr.univ_amu.iut.exercice1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CounterMainTest {
+
+}
---
+++ src/test/java/fr/univ_amu/iut/exercice2/LoginMainTest.java
@@ -0,0 +1,7 @@
+package fr.univ_amu.iut.exercice2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginMainTest {
+
+}
